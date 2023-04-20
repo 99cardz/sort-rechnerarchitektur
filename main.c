18ccd59f44ac9c1ee754f499d5656213175731e7
99cardz <jonathan.schulze.de@gmail.com>
2023-04-20 20:33:24 +0200

    flags

--- main.c
+++ main.c
@@ -1,7 +1,17 @@
-#include "bubble.h"
-#include "quick.h"
-#include "insert.h"
-#include "merge.h"
+#include <stdlib.h>
+
+#ifdef BUBBLE
+    #include "bubble.h"
+#endif
+#ifdef QUICK
+    #include "quick.h"
+#endif
+#ifdef INSERT
+    #include "insert.h"
+#endif
+#ifdef MERGE
+    #include "merge.h"
+#endif
 
 int compare(int a, int b) {
     if (a > b) {
@@ -16,10 +26,20 @@ int compare(int a, int b) {
 
 int main() {
     const int len = 20;
-    const int a[len] = { 7, 19, 10, 6, 12, 17, 3, 16, 11, 15, 2, 4, 18, 14, 1, 5, 8, 9, 0, 13 };
-    const int * ptr = a;
+    const int arr[len] = { 7, 19, 10, 6, 12, 17, 3, 16, 11, 15, 2, 4, 18, 14, 1, 5, 8, 9, 0, 13 };
 
-    quicksort(ptr, len, sizeof(int), compare);
+#ifdef QUICK
+    quicksort(arr, len, sizeof(int), compare);
+#endif
+#ifdef BUBBLE
+    bubblesort(arr, len, sizeof(int), compare);
+#endif
+#ifdef MERGE
+    merge(arr, len, sizeof(int), compare);
+#endif
+#ifdef INSERT
+    insertionsort(arr, len, sizeof(int), compare);
+#endif
 
     return 0;
 }
