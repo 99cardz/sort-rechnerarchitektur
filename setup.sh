fba61367a13aaf01de3dca2a21dddefd7bf55cd7
99cardz <jonathan.schulze.de@gmail.com>
2023-04-21 11:29:11 +0200

    setup script generates all test files and executables

--- setup.sh
+++ setup.sh
@@ -1,72 +1,18 @@
 
 # Create directories for object files and disassembled files
-mkdir -p obj dis
+mkdir -p obj dis bin
 
-#!/bin/bash
+# Define the preprocessor flags and optimization levels
+FLAGS=("QUICK" "MERGE" "INSERT" "BUBBLE")
+OPTIMIZATIONS=("O0" "O1" "O2" "O3")
 
-# Create directories for object files and disassembled files
-mkdir -p obj dis
+# Loop through each preprocessor flag and optimization level
+for FLAG in "${FLAGS[@]}"; do
+  for OPT in "${OPTIMIZATIONS[@]}"; do
+    # Compile main.c with the current flag and optimization level
+    gcc -D$FLAG -$OPT -c main.c -o obj/main-${FLAG}-${OPT}.o
+    objdump -d obj/main-${FLAG}-${OPT}.o > dis/main-${FLAG}-${OPT}.s
+    gcc main.c -o bin/main-${FLAG}-${OPT}
+  done
+done
 
-# Compile main.c with QUICK flag and -O0 optimization
-gcc -DQUICK -O0 -c main.c -o obj/main_QUICK_O0.o
-objdump -d obj/main_QUICK_O0.o > dis/main_QUICK_O0.s
-
-# Compile main.c with QUICK flag and -O1 optimization
-gcc -DQUICK -O1 -c main.c -o obj/main_QUICK_O1.o
-objdump -d obj/main_QUICK_O1.o > dis/main_QUICK_O1.s
-
-# Compile main.c with QUICK flag and -O2 optimization
-gcc -DQUICK -O2 -c main.c -o obj/main_QUICK_O2.o
-objdump -d obj/main_QUICK_O2.o > dis/main_QUICK_O2.s
-
-# Compile main.c with QUICK flag and -O3 optimization
-gcc -DQUICK -O3 -c main.c -o obj/main_QUICK_O3.o
-objdump -d obj/main_QUICK_O3.o > dis/main_QUICK_O3.s
-
-# Compile main.c with MERGE flag and -O0 optimization
-gcc -DMERGE -O0 -c main.c -o obj/main_MERGE_O0.o
-objdump -d obj/main_MERGE_O0.o > dis/main_MERGE_O0.s
-
-# Compile main.c with MERGE flag and -O1 optimization
-gcc -DMERGE -O1 -c main.c -o obj/main_MERGE_O1.o
-objdump -d obj/main_MERGE_O1.o > dis/main_MERGE_O1.s
-
-# Compile main.c with MERGE flag and -O2 optimization
-gcc -DMERGE -O2 -c main.c -o obj/main_MERGE_O2.o
-objdump -d obj/main_MERGE_O2.o > dis/main_MERGE_O2.s
-
-# Compile main.c with MERGE flag and -O3 optimization
-gcc -DMERGE -O3 -c main.c -o obj/main_MERGE_O3.o
-objdump -d obj/main_MERGE_O3.o > dis/main_MERGE_O3.s
-
-# Compile main.c with INSERT flag and -O0 optimization
-gcc -DINSERT -O0 -c main.c -o obj/main_INSERT_O0.o
-objdump -d obj/main_INSERT_O0.o > dis/main_INSERT_O0.s
-
-# Compile main.c with INSERT flag and -O1 optimization
-gcc -DINSERT -O1 -c main.c -o obj/main_INSERT_O1.o
-objdump -d obj/main_INSERT_O1.o > dis/main_INSERT_O1.s
-
-# Compile main.c with INSERT flag and -O2 optimization
-gcc -DINSERT -O2 -c main.c -o obj/main_INSERT_O2.o
-objdump -d obj/main_INSERT_O2.o > dis/main_INSERT_O2.s
-
-# Compile main.c with INSERT flag and -O3 optimization
-gcc -DINSERT -O3 -c main.c -o obj/main_INSERT_O3.o
-objdump -d obj/main_INSERT_O3.o > dis/main_INSERT_O3.s
-
-# Compile main.c with BUBBLE flag and -O0 optimization
-gcc -DBUBBLE -O0 -c main.c -o obj/main_BUBBLE_O0.o
-objdump -d obj/main_BUBBLE_O0.o > dis/main_BUBBLE_O0.s
-
-# Compile main.c with BUBBLE flag and -O0 optimization
-gcc -DBUBBLE -O1 -c main.c -o obj/main_BUBBLE_O1.o
-objdump -d obj/main_BUBBLE_O1.o > dis/main_BUBBLE_O1.s
-
-# Compile main.c with BUBBLE flag and -O0 optimization
-gcc -DBUBBLE -O2 -c main.c -o obj/main_BUBBLE_O2.o
-objdump -d obj/main_BUBBLE_O2.o > dis/main_BUBBLE_O2.s
-
-# Compile main.c with BUBBLE flag and -O3 optimization
-gcc -DBUBBLE -O3 -c main.c -o obj/main_BUBBLE_O3.o
-objdump -d obj/main_BUBBLE_O3.o > dis/main_BUBBLE_O3.s
