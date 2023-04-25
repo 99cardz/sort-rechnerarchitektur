888f4cedf21fa5275800f1f8fff90a0b12a90c67
99cardz <jonathan.schulze.de@gmail.com>
2023-04-25 10:11:38 +0200

    sed mnemonic extract to csv

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 
 # Create directories for object files and disassembled files
-mkdir -p obj dis bin
+mkdir -p obj dis bin csv
 
 # Define the preprocessor flags and optimization levels
 FLAGS=("QUICK" "MERGE" "INSERT" "BUBBLE")
@@ -12,7 +12,9 @@ for FLAG in "${FLAGS[@]}"; do
     # Compile main.c with the current flag and optimization level
     gcc -D$FLAG -$OPT -c main.c -o obj/main-${FLAG}-${OPT}.o
     objdump -d obj/main-${FLAG}-${OPT}.o > dis/main-${FLAG}-${OPT}.s
+    sed  -En 's/^ +[0-9a-f]+: ([0-9a-f][0-9a-f] )+ *\t([a-z]+)\t.*/\2/p' dis/main-${FLAG}-${OPT}.s > csv/main-${FLAG}-${OPT}.csv
     gcc main.c -o bin/main-${FLAG}-${OPT}
+    
   done
 done
 
