38e7dcfb696917f0abe03128d79df9afda4857ca
99cardz <jonathan.schulze.de@gmail.com>
2023-04-27 12:32:20 +0200

    cleanup

--- müller/bin/CMakeFiles/demosort_mini.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for demosort_mini.
-# This may be replaced when dependencies are built.
--- müller/doit.sh
+++
@@ -1,3 +0,0 @@
-cmake -B bin -S .
-cmake --build bin
-## reset;cmake --build bin && valgrind --track-origins=yes  bin/demo
